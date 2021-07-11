5141e0d9df7074f98284775ba90c7c41459a5807
Jacob Pratt <jacob@jhpratt.dev>
2021-07-11 14:50:42 -0400

    Directly construct iterator

--- tests/integration/parse_format_description.rs
+++ tests/integration/parse_format_description.rs
@@ -1,34 +1,36 @@
 mod iterator {
+    use std::array::IntoIter;
+
     use time::format_description::modifier::{
         MonthRepr, Padding, SubsecondDigits, WeekNumberRepr, WeekdayRepr, YearRepr,
     };
 
-    pub(super) fn padding() -> Vec<(Padding, &'static str)> {
-        vec![
+    pub(super) fn padding() -> impl Iterator<Item = (Padding, &'static str)> {
+        IntoIter::new([
             (Padding::Space, "padding:space"),
             (Padding::Zero, "padding:zero"),
             (Padding::None, "padding:none"),
-        ]
+        ])
     }
 
-    pub(super) fn hour_is_12_hour_clock() -> Vec<(bool, &'static str)> {
-        vec![(false, "repr:24"), (true, "repr:12")]
+    pub(super) fn hour_is_12_hour_clock() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([(false, "repr:24"), (true, "repr:12")])
     }
 
-    pub(super) fn period_is_uppercase() -> Vec<(bool, &'static str)> {
-        vec![(true, "case:upper"), (false, "case:lower")]
+    pub(super) fn period_is_uppercase() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([(true, "case:upper"), (false, "case:lower")])
     }
 
-    pub(super) fn month_repr() -> Vec<(MonthRepr, &'static str)> {
-        vec![
+    pub(super) fn month_repr() -> impl Iterator<Item = (MonthRepr, &'static str)> {
+        IntoIter::new([
             (MonthRepr::Numerical, "repr:numerical"),
             (MonthRepr::Long, "repr:long"),
             (MonthRepr::Short, "repr:short"),
-        ]
+        ])
     }
 
-    pub(super) fn subsecond_digits() -> Vec<(SubsecondDigits, &'static str)> {
-        vec![
+    pub(super) fn subsecond_digits() -> impl Iterator<Item = (SubsecondDigits, &'static str)> {
+        IntoIter::new([
             (SubsecondDigits::One, "digits:1"),
             (SubsecondDigits::Two, "digits:2"),
             (SubsecondDigits::Three, "digits:3"),
@@ -39,50 +41,50 @@ mod iterator {
             (SubsecondDigits::Eight, "digits:8"),
             (SubsecondDigits::Nine, "digits:9"),
             (SubsecondDigits::OneOrMore, "digits:1+"),
-        ]
+        ])
     }
 
-    pub(super) fn weekday_repr() -> Vec<(WeekdayRepr, &'static str)> {
-        vec![
+    pub(super) fn weekday_repr() -> impl Iterator<Item = (WeekdayRepr, &'static str)> {
+        IntoIter::new([
             (WeekdayRepr::Short, "repr:short"),
             (WeekdayRepr::Long, "repr:long"),
             (WeekdayRepr::Sunday, "repr:sunday"),
             (WeekdayRepr::Monday, "repr:monday"),
-        ]
+        ])
     }
 
-    pub(super) fn week_number_repr() -> Vec<(WeekNumberRepr, &'static str)> {
-        vec![
+    pub(super) fn week_number_repr() -> impl Iterator<Item = (WeekNumberRepr, &'static str)> {
+        IntoIter::new([
             (WeekNumberRepr::Iso, "repr:iso"),
             (WeekNumberRepr::Sunday, "repr:sunday"),
             (WeekNumberRepr::Monday, "repr:monday"),
-        ]
+        ])
     }
 
-    pub(super) fn year_repr() -> Vec<(YearRepr, &'static str)> {
-        vec![
+    pub(super) fn year_repr() -> impl Iterator<Item = (YearRepr, &'static str)> {
+        IntoIter::new([
             (YearRepr::Full, "repr:full"),
             (YearRepr::LastTwo, "repr:last_two"),
-        ]
+        ])
     }
 
-    pub(super) fn year_is_iso_week_based() -> Vec<(bool, &'static str)> {
-        vec![(false, "base:calendar"), (true, "base:iso_week")]
+    pub(super) fn year_is_iso_week_based() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([(false, "base:calendar"), (true, "base:iso_week")])
     }
 
-    pub(super) fn sign_is_mandatory() -> Vec<(bool, &'static str)> {
-        vec![(false, "sign:automatic"), (true, "sign:mandatory")]
+    pub(super) fn sign_is_mandatory() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([(false, "sign:automatic"), (true, "sign:mandatory")])
     }
 
-    pub(super) fn weekday_is_one_indexed() -> Vec<(bool, &'static str)> {
-        vec![(true, "one_indexed:true"), (false, "one_indexed:false")]
+    pub(super) fn weekday_is_one_indexed() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([(true, "one_indexed:true"), (false, "one_indexed:false")])
     }
 
-    pub(super) fn case_sensitive() -> Vec<(bool, &'static str)> {
-        vec![
+    pub(super) fn case_sensitive() -> impl Iterator<Item = (bool, &'static str)> {
+        IntoIter::new([
             (true, "case_sensitive:true"),
             (false, "case_sensitive:false"),
-        ]
+        ])
     }
 }
 
